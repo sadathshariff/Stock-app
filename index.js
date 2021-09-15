fe83b8aea60a2f1e00dc63d5397cc4c8843cd46f
sadath <sadathshariff65@gmail.com>
2021-09-15 10:52:26 +0530

    Fix:validations for the negative values

--- index.js
+++ index.js
@@ -5,23 +5,25 @@ const submitBtn = document.querySelector("#submit-btn");
 const outputDiv = document.querySelector(".output-div");
 
 function calculateProfitAndLoss(initialPrice, noOfStocks, currentPrice) {
-  if (initialPrice > currentPrice) {
-    let loss = (initialPrice - currentPrice) * noOfStocks;
-    let lossPercentage = (loss / initialPrice) * 100;
-    showOutputMsg(
-      `Loss is ₹${loss} and Loss Percentage is ${lossPercentage}%`,
-      "red"
-    );
-  } else if (currentPrice > initialPrice) {
-    let profit = ((currentPrice - initialPrice) * noOfStocks).toFixed(2);
+  if (initialPrice > 0 && noOfStocks > 0 && currentPrice > 0) {
+    if (initialPrice > currentPrice) {
+      let loss = (initialPrice - currentPrice) * noOfStocks;
+      let lossPercentage = ((loss / initialPrice) * 100).toFixed(2);
+      showOutputMsg(
+        `Loss is ₹${loss} and Loss Percentage is ${lossPercentage}%`,
+        "red"
+      );
+    } else if (currentPrice > initialPrice) {
+      let profit = ((currentPrice - initialPrice) * noOfStocks).toFixed(2);
 
-    let profitPercentage = ((profit / initialPrice) * 100).toFixed(2);
-    showOutputMsg(
-      `The Profit is ₹${profit} and the profit percentage is ${profitPercentage}%`,
-      "green"
-    );
-  } else {
-    showOutputMsg("No pain No gain");
+      let profitPercentage = ((profit / initialPrice) * 100).toFixed(2);
+      showOutputMsg(
+        `The Profit is ₹${profit} and the profit percentage is ${profitPercentage}%`,
+        "green"
+      );
+    } else {
+      showOutputMsg("No pain No gain");
+    }
   }
 }
 
